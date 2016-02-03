2b018f0cce56e62197ceeb5e12a0d06c05503547
Lesliedao <Leslie@wcw-stud-145-109-4-42.wireless.uva.nl>
2016-02-03 14:20:23 +0100

    added functionality to tvscraper.py, does not clear all checks yet. might have to change the way the scraper stores the info

--- Homework/week-1/tvscraper.py
+++ Homework/week-1/tvscraper.py
@@ -30,8 +30,62 @@ def extract_tvseries(dom):
     # HIGHEST RANKING TV-SERIES
     # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
     # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
+    
+    scrape = []
 
-    return []  # replace this line as well as appropriate
+	# scrape de titels van de series
+    for img in dom.by_tag("img"):
+    	title = img.attrs.get("title","")
+    	title = title.encode("utf-8")	
+    	title = title[:title.find("(") - 1]
+    	if title == "":
+    		pass
+    	else:
+    		scrape.append(title)
+    
+    # het eerste element van de list is "Go to IMDb Pro"	
+    scrape.pop(0)
+    
+    # scrape de rankings van de series
+    for span in dom.by_class("value"):
+    	span = span.content.encode("utf-8")
+    	scrape.append(span)
+    
+    # scrape de genres
+    for genres in dom.by_class("genre"):
+    	genlist = []
+    	genres = DOM(genres.content)
+    	for genre in genres:
+    		try:
+    			genlist.append(genre.content.encode("utf-8"))
+    		except AttributeError:
+    			pass
+    			
+    	genres = ", ".join(genlist)
+    	scrape.append(genres)
+    	
+    # scrape de acteurs
+    for actors in dom.by_class("credit"):
+    	actorlist = []
+    	actors = DOM(actors.content)
+    	for actor in actors:
+    		try:
+    			actorlist.append(actor.content.encode("utf-8"))
+    		except AttributeError:
+    			pass
+    	
+    	actors = ", ".join(actorlist)
+    	scrape.append(actors)
+    
+    # scrape de runtimes
+    for runtime in dom.by_class("runtime"):
+    	runtime = runtime.content
+    	runtime = runtime[:runtime.find(" ")]
+    	scrape.append(runtime)
+    	
+    #print scrape
+    #print len(scrape)
+    return scrape  # replace this line as well as appropriate
 
 
 def save_csv(f, tvseries):
@@ -40,7 +94,10 @@ def save_csv(f, tvseries):
     '''
     writer = csv.writer(f)
     writer.writerow(['Title', 'Ranking', 'Genre', 'Actors', 'Runtime'])
-
+    for i in range(50):
+    	row = [tvseries[i], tvseries[i + 50], tvseries[i + 100], tvseries[i + 150], tvseries[i + 200]]
+    	writer.writerow(row)
+    	
     # ADD SOME CODE OF YOURSELF HERE TO WRITE THE TV-SERIES TO DISK
 
 if __name__ == '__main__':
@@ -55,6 +112,7 @@ if __name__ == '__main__':
 
     # Parse the HTML file into a DOM representation
     dom = DOM(html)
+    # print dom
 
     # Extract the tv series (using the function you implemented)
     tvseries = extract_tvseries(dom)
