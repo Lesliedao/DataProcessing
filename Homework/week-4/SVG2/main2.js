99def516abde51ceded6f2a94b4152b74412a1df
Lesliedao <l.dao@hotmail.com>
2016-02-23 15:21:23 +0100

    added color mapping

--- Homework/week-4/SVG2/main2.js
+++ Homework/week-4/SVG2/main2.js
@@ -1,13 +1,36 @@
 /* use this to test out your function */
 window.onload = function() {
-    // Verander Frankrijk naar geel
- 	changeColor("fr", "#F0FF00");
-    // Verander Italie naar groen
-    changeColor("it", "#00AE0C");
-    // Verander Griekenland naar paars
-    changeColor("gr", "#AE009E");
-    // Verander Oostenrijk naar roze
-    changeColor("at", "#FF82FA");
+    
+    var rawdata = document.getElementById("JSON");
+    var dataObject = JSON.parse(rawdata.value);
+    
+    var range1 = "#EDF8E9", range2 = "#BAE4B3", range3 = "#74C476", range4 = "#31A354", range5 = "#006D2C";
+    for (var i = 0; i < dataObject['points'].length; i++)
+    {
+        var code = countryCode(dataObject['points'][i][0]);
+        if (code != 'xxx')
+        {
+            var selector = document.getElementById(code);
+            if (selector !== null)
+            {
+                var percentage = Number(dataObject['points'][i][1].replace(/,/g, '.'));
+                var color;
+                if (percentage < 20)
+                    color = range1;
+                else if (percentage < 40)
+                    color = range2;
+                else if (percentage < 60)
+                    color = range3;
+                else if (percentage < 80)
+                    color = range4;
+                else
+                    color = range5;
+                
+                changeColor(code, color);
+            }
+        }
+    }
+    //document.getElementsByClassName("gb")[1].style.fill = range1;
 }
 
 /* changeColor takes a path ID and a color (hex value)
@@ -21,4 +44,20 @@ function changeColor(id, color) {
     / Bron: http://www.w3schools.com/js/js_htmldom_css.asp
     */
         document.getElementById(id).style.fill = color;
+}
+
+function countryCode(country)
+{
+    // Placeholder
+    var code = 'xxx';
+    for (var i = 0; i < country_codes.length; i++)
+    {
+        if (country.toLowerCase() == country_codes[i][2].toLowerCase())
+        {
+            code = country_codes[i][0];
+            break;
+        }
+    }
+    
+    return code;
 }
