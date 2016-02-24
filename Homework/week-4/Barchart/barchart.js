c1041206ecef0ae07d4098a8a7da6c51d594d95c
Lesliedao <l.dao@hotmail.com>
2016-02-24 16:30:50 +0100

    edited to refer to rain instead

--- Homework/week-4/Barchart/barchart.js
+++ Homework/week-4/Barchart/barchart.js
@@ -2,12 +2,12 @@
 / Leslie Dao
 / 10561234
 /
-/ Laadt de JSON gemiddelde maandtemperatuur in de Bilt in (2014) met D3
-/ Bron: https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_260_tg.txt
+/ Laadt de JSON gemiddelde neerslag per maand in Nijverdal in (2014) met D3
+/ Bron: http://weerstationnijverdal.nl/metingen/historie/Gemiddelde%20neerslag%20per%20maand.pdf
 */
 
 var data;
-d3.json("avgtemp2014.json", function(error, json) {
+d3.json("avgrain2014.json", function(error, json) {
 	if (error) return console.warn(error);
 	data = json;
 	console.log(data);
