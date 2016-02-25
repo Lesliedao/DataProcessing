b68c65ea1760ce44c786bccdae1494300efef140
Lesliedao <l.dao@hotmail.com>
2016-02-25 22:31:09 +0100

    edited files to sum of rain per month

--- Homework/week-4/Barchart/barchart.js
+++ Homework/week-4/Barchart/barchart.js
@@ -2,13 +2,13 @@
 / Leslie Dao
 / 10561234
 /
-/ Laadt de JSON gemiddelde neerslag per maand in Nijverdal in (2014) met D3
+/ Laadt de JSON neerslagsom per maand in Nijverdal in (2014) met D3
 / Bron: http://weerstationnijverdal.nl/metingen/historie/Gemiddelde%20neerslag%20per%20maand.pdf
 */
 
 var data;
-d3.json("avgrain2014.json", function(error, json) {
+d3.json("totalrain2014.json", function(error, json) {
 	if (error) return console.warn(error);
-	data = json;
-	console.log(data);
+	data = json.data;
+    console.log(data);
 });
